bf68fd75b31751b34bc0bdb256bfac16925704c1
Justin Bogantes <justin.bogantes99@gmail.com>
2022-05-17 18:27:50 -0600

    .

---
+++ TC/target/debug/TC.d
@@ -0,0 +1,1 @@
+/home/justin/Documentos/GitHub/ThreadCity/TC/target/debug/TC: /home/justin/Documentos/GitHub/ThreadCity/TC/src/main.rs
---
+++ TC/target/debug/deps/TC-707ffe59d0bd306c.d
@@ -0,0 +1,5 @@
+/home/justin/Documentos/GitHub/ThreadCity/TC/target/debug/deps/TC-707ffe59d0bd306c: src/main.rs
+
+/home/justin/Documentos/GitHub/ThreadCity/TC/target/debug/deps/TC-707ffe59d0bd306c.d: src/main.rs
+
+src/main.rs:
